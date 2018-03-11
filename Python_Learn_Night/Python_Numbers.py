689aeff74b916456f1d6d846ffbba6dd9a69da8f
Shivakumar Mahakali <mahakalim@yahoo.com>
2018-03-11 19:19:45 -0400

    Added Numbers and Strings.py

---
+++ Python_Learn_Night/Python_Numbers.py
@@ -0,0 +1,10 @@
+# Single-line comments in Python start with "#"
+
+# Python supports integers, floats, and complex numbers:
+large_number = (2**100)
+print(large_number)
+
+print(59/37)
+
+complex_num = 3 + 5j
+
---
+++ Python_Learn_Night/Python_Strings.py
@@ -0,0 +1,12 @@
+# Strings in Python
+
+string1 = "Hello World!"
+
+# Strings in Python are immutable.
+
+# Indexing and slicing strings. Print "W"
+print(string1["NUMBER HERE"])
+
+# concatenating strings
+
+# Escape characters
