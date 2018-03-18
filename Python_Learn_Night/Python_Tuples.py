236260d7f5c23af7152c8116624052fde48cdb06
Shivakumar Mahakali <mahakalim@yahoo.com>
2018-03-18 16:00:32 -0400

    Added Python_Tuples.py

---
+++ Python_Learn_Night/Python_Tuples.py
@@ -0,0 +1,26 @@
+# Tuples are Python's Immutable version of the Sequence Data Type
+
+tuple1 = (1, 2, "Hello")
+tuple2 = (100, )
+tuple3 = 10, 20, 30
+empty_tuple = tuple()
+
+# because they are immutable we cannot add or remove individual elements
+del tuple2
+
+# We still have the basic methods from lists that can be applied to tuples
+# len()
+# concatenation (+)
+# repetition (*)
+# "in" method
+
+# Why use tuples?
+
+
+
+
+
+
+
+
+
