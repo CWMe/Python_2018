af307529b595f6992e6735ece7b52e13204515dd
Shivakumar Mahakali <mahakalim@yahoo.com>
2018-04-07 14:24:05 -0400

    Added comments to Python_Tuples.py

--- Python_Learn_Night/Python_Tuples.py
+++ Python_Learn_Night/Python_Tuples.py
@@ -15,12 +15,16 @@ del tuple2
 # "in" method
 
 # Why use tuples?
+# - Certain major functions in different modules return data in the form of a tuple
+# - Tuples are comparable:
+if (0, 1, 2) < (0, 10, 20):
+    print("The above statement evaluated to true.")
 
+# How? - comparing each element to see which is greater, if tied then moves to the next, and continues.
 
+# Tuple assignments:
+x, y = ["hello", "world"]
 
-
-
-
-
-
-
+# Dictionaries and Tuples
+# dictionaries have a method called .items() which return the key-value pair in the form of a tuple.
+# since tuples are comparable the dictionary elements get ordered.
