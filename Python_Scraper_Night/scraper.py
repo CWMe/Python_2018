1d60ea65a95495ca6603dfc5784d44ca1328c6e0
Diego Ruiz <druiz@alfajores.lan>
2018-04-27 00:22:03 -0400

    cleanup

--- Python_Scraper_Night/scraper.py
+++ Python_Scraper_Night/scraper.py
@@ -1,28 +1,36 @@
 from bs4 import BeautifulSoup
 from operator import itemgetter
 
-with open("book.html") as fp:
-    soup = BeautifulSoup(fp, "html.parser")
+def main():
+    #open file
+    with open("book.html") as fp:
+        soup = BeautifulSoup(fp, "html.parser")
 
-common_words = ["the", "of", "a", "and", "to", "in", "was"]
-dictionary = {}
+    #setup
+    common_words = ["the", "of", "a", "and", "to", "in", "was"]
+    dictionary = {}
+
+    #build dictionary
+    for child in soup.find(id="book").children:
+        if child.name == "p" and child.string:
+            words = child.string.split()
+            for word in words:
+                word = word.lower()
+                if word is not None and word not in common_words and dictionary.get(word) is None:
+                    dictionary[word] = 1
+                elif word is not None and word not in common_words and dictionary.get(word) is not None:
+                    dictionary[word]+= 1
 
 
-for child in soup.find(id="book").children:
-    if child.name == "p" and child.string:
-        words = child.string.split()
-        for word in words:
-            word = word.lower()
-            if word is not None and word not in common_words and dictionary.get(word) is None:
-                dictionary[word] = 1
-            elif word is not None and word not in common_words and dictionary.get(word) is not None:
-                dictionary[word]+= 1
+    #sort
+    sorted_x = sorted(dictionary.items(), key = itemgetter(1), reverse = True)
 
 
-sorted_x = sorted(dictionary.items(), key = itemgetter(1), reverse = True)
+    #output
+    print(f"{sorted_x[0]} is 1st most common")
+    print(f"{sorted_x[1]} is 2nd most common")
+    print(f"{sorted_x[2]} is 3rd most common")
+    print(f"{sorted_x[3]} is 4th most common")
+    print(f"{sorted_x[4]} is 5th most common")
 
-print(f"{sorted_x[0]} is 1st most common")
-print(f"{sorted_x[1]} is 2nd most common")
-print(f"{sorted_x[2]} is 3rd most common")
-print(f"{sorted_x[3]} is 4th most common")
-print(f"{sorted_x[4]} is 5th most common")
+main()
